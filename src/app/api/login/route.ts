fecf1236848f1ce2bdd2c44ea12bb8fee81a9e64
Adriano-Reis-Eng <adrisilva.dev@gmail.com>
2025-05-04 23:54:58 -0300

    Correção Get route.js

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -26,16 +26,16 @@ export async function POST(request: NextRequest) {
   return NextResponse.json(data, { status: response.status });
 } 
 
-export async function GET(request: NextRequest) {
-  const { searchParams } = new URL(request.url);
-  const queryString = searchParams.toString();
-  const Url = `${url}?${queryString}`;
-  const response = await fetch(Url);
+// export async function GET(request: NextRequest) {
+//   const { searchParams } = new URL(request.url);
+//   const queryString = searchParams.toString();
+//   const Url = `${url}?${queryString}`;
+//   const response = await fetch(Url);
   
-  if (!response.ok) {
-    return NextResponse.json({ error: "Erro ao buscar dados" }, { status: 500 });
-  }
+//   if (!response.ok) {
+//     return NextResponse.json({ error: "Erro ao buscar dados" }, { status: 500 });
+//   }
 
-  const data = await response.json();
-  return NextResponse.json(data);
-}
+//   const data = await response.json();
+//   return NextResponse.json(data);
+// }
