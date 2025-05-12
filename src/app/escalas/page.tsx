6970666523e0da237fa5e8a6b37e6b479175a68a
Adriano-Reis-Eng <adrisilva.dev@gmail.com>
2025-05-12 08:59:49 -0300

    Modulos busca mot/cob

--- src/app/escalas/page.tsx
+++ src/app/escalas/page.tsx
@@ -1,174 +1,155 @@
 'use client'
 import { useState, useEffect } from "react";
-import { validToken, getToken } from "../../utils/jwt";
-import InputBtn from "../components/InputBtn";
+import { payloadToken } from "../../utils/jwt";
+import Cookies from "js-cookie";
+//import InputBtn from "../components/inputBtn/InputBtn";
 import styles from "./Escalas.module.css"
+import BackBtn from "../components/backBtn/BackBtn";
+//import Usuario from "../components/usuario/usuario";
+//import AvisoForm from "../cadastro/AvisoForm";
 
 interface Escala {
-    servico: string;
-    linha: string;
-    inicio: string;
-    VG2: string;
-    VG3: string;
-    VG4: string;
-    VG5: string;
-    VG6: string;
-    VG7: string;
-    VG8: string;
-    VG9: string;
-    VG10: string;
-    final: string;
-    dia: string;
-    tipo: string;
+    idEscala: string;
+    semanainicio: string;
+    semanafim: string;
+    sabadoinicio: string;
+    sabadofim: string;
+    domingoinicio: string;
+    domingofim: string;
+    suplementarDU: string;
+    suplementarS: string;
 }
 
 export default function EscalaPage() {
+    const [message, setMessage] = useState("");
     const [cracha, setCracha] = useState("");
     const [nome, setNome] = useState("");
     const [escala, setEscala] = useState<Escala[]>([]);
-    const [loading, setLoading] = useState(false);
-    const [erro, setError] = useState<string | null>(null);
-    const [tipoEscala, setTipoEscala] = useState("semana");
-    const [dia_semana, setDiaSemana] = useState("");
+    //const [loading, setLoading] = useState(false);
+    const [erro, setError] = useState<string | null>(null);    
 
     useEffect(() => {
-        const token = sessionStorage.getItem("token");
+        const fetchData = async () => {
+            setMessage('Buscando...');
+            const token = Cookies.get("token");
+            let func = "";
+            if (token) {
+                const user = payloadToken(token);
+                func = user?.cracha || "";
+                if (user?.cracha) {
+                    setCracha(user.cracha);
+                }
+                if (user?.nome) {
+                    setNome(user.nome);
+                }
+            }
 
-        if (!token || !validToken(token)) {
-            setError("Necessário fazer o login.");
-            return;
-        }
-
-        const payload = getToken(token);
-
-        if (payload?.cracha) {
-            setCracha(payload.cracha);
-        }
-        if (payload?.nome) {
-            setNome(payload.nome);
-        }
-    }, []);
-
-    async function handleSubmit(e: React.FormEvent) {
-        e.preventDefault();        
-
-        const token = sessionStorage.getItem("token");
-        const valdToken = validToken(token)        
-        
-        if (valdToken === true) {
-            setLoading(true);
-            setError("");
             try {
-                console.log(sessionStorage.getItem("token"))
                 const response = await fetch("/api/login", {
                     method: "POST",
                     headers: { "Content-Type": "application/json" },
                     body: JSON.stringify({
-                        funcao: "Escala",
-                        token: sessionStorage.getItem("token"),
+                        funcao: "ListaFerias",
+                        token: Cookies.get("token"),
+                        sheet: "macope",
                         data: {
-                            cracha,
-                            tipoEscala
+                            cracha: func
                         }
                     })
-                })
+                });
                 const result = await response.json();
-                console.log(result)
                 if (result.success == false) {
-                    setDiaSemana("Sem escala programada.");
-                    setEscala([result]);
+                    setMessage('Sem periodo de férias programado.');
                 }
                 else if (result.success == true) {
-                    setEscala([result.message]);
-                    setDiaSemana(result.message.dia || "");
-                    setError("");
+                    setEscala(result.message);
+                    setMessage("");
                 }
             } catch {
-                setError("Erro ao conectar com o servidor.");
-            } finally {
-                setLoading(false);
+                setMessage("Erro ao conectar com o servidor.");
+                setError("")
             }
-        }
-        else{
-            setLoading(false);
-        }
-    }
+        };
+
+        fetchData();
+    }, []);    
+
+    // async function handleSubmit(e: React.FormEvent) {
+    //     e.preventDefault();
+    //     const token = Cookies.get("token");
+    //     const valdToken = validToken(token || "")
+    //     if (valdToken === true) {
+    //         setLoading(true);
+    //         setError("");
+    //         try {
+
+    //             const response = await fetch("/api/login", {
+    //                 method: "POST",
+    //                 headers: { "Content-Type": "application/json" },
+    //                 body: JSON.stringify({
+    //                     funcao: "Escala",
+    //                     token: sessionStorage.getItem("token"),
+    //                     data: {
+    //                         cracha,
+    //                         tipoEscala
+    //                     }
+    //                 })
+    //             })
+    //             const result = await response.json();
+    //             console.log(result)
+    //             if (result.success == false) {
+    //                 setDiaSemana("Sem escala programada.");
+    //                 setEscala([result]);
+    //             }
+    //             else if (result.success == true) {
+    //                 setEscala([result.message]);
+    //                 setDiaSemana(result.message.dia || "");
+    //                 setError("");
+    //             }
+    //         } catch {
+    //             setError("Erro ao conectar com o servidor.");
+    //         } finally {
+    //             setLoading(false);
+    //         }
+    //     }
+    //     else {
+    //         setLoading(false);
+    //     }
+    // }
     return (
         <main className={styles.main}>
-            <div className={styles.container}>
+            <div className={styles.containerUser}>                
+                <section className={styles.containerBtn}>
+                    <BackBtn />
+                </section>
                 {erro ? (
                     <p className={styles.errorMsg}>{erro}</p>
                 ) : (
-                    <h2 className={styles.usuario}>{nome} {cracha}</h2>
+                    <h2 className={styles.usuario}>{nome}<br /> {cracha}</h2>
                 )}
-                <div className={styles.content}>
-                    <div className={styles.formGroup}>
-                        <h2 className={styles.title}>Selecione o tipo de escala:</h2>
-                        <select
-                            className={styles.imputEscala}
-                            id="escala"
-                            name="escala"
-                            value={tipoEscala}
-                            onChange={e => setTipoEscala(e.target.value)}>
-                            <option value="suplementar">Suplementar</option>
-                            <option value="semana">Dias uteis</option>
-                            <option value="sabado">Sábado</option>
-                            <option value="domingo">Domingo</option>
-                        </select>
-                        <InputBtn type="submit" onClick={handleSubmit} disabled={loading}>
-                            {loading ? "Buscar..." : "Buscar"}
-                        </InputBtn>                        
-                    </div>
-                </div>
-                
             </div>
-            {escala.length > 0 && (
-                <div className={styles.resultado}>
-                    <h3>Escala: {dia_semana}</h3>
-                    <table className={styles.tabelaEscala}>
-                        <thead>
-                            <tr>
-                                <th>Serviço</th>
-                                <th>LInha</th>
-                                <th>Início</th>
-                                <th>VG2</th>
-                                <th>VG3</th>
-                                <th>VG4</th>
-                                <th>VG5</th>
-                                <th>VG6</th>
-                                <th>VG7</th>
-                                <th>VG8</th>
-                                <th>VG9</th>
-                                <th>VG10</th>
-                                <th>Final</th>
-                                <th>Dia</th>
-                                <th>Tipo</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {escala.map((item, idx) => (
-                                <tr key={idx}>
-                                    <td>{item.servico}</td>
-                                    <td>{item.linha}</td>
-                                    <td>{item.inicio}</td>
-                                    <td>{item.VG2}</td>
-                                    <td>{item.VG3}</td>
-                                    <td>{item.VG4}</td>
-                                    <td>{item.VG5}</td>
-                                    <td>{item.VG6}</td>
-                                    <td>{item.VG7}</td>
-                                    <td>{item.VG8}</td>
-                                    <td>{item.VG9}</td>
-                                    <td>{item.VG10}</td>
-                                    <td>{item.final}</td>
-                                    <td>{item.dia}</td>
-                                    <td>{item.tipo}</td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
-            )}
+            <div className={styles.content}>
+                {message && <p className={styles.successMsg}>{message}</p>}
+                {escala.length > 0 && (
+
+                    <div className={styles.cardContainer}>
+                        <h2>Escala:</h2>
+                        {escala.map((item, idx) => (
+                            <div className={styles.card} key={idx}>
+                                <p>Escala: {item.idEscala}</p>
+                                <p>Início (Semana): {item.semanainicio}</p>
+                                <p>Final (Semana): {item.semanafim}</p>
+                                <p>Início (Sábado): {item.sabadoinicio}</p>
+                                <p>Final (Sábado): {item.sabadofim}</p>
+                                <p>Início (Domingo) {item.sabadoinicio}</p>
+                                <p>Final (Domingo): {item.domingofim}</p>
+                                <p>Suplementar 2º/6º:{item.suplementarDU}</p>
+                                <p>Suplementar Sábado: {item.suplementarS}</p>
+                            </div>
+                        ))}
+                    </div>
+                )}
+            </div>
         </main>
     );
 } 
