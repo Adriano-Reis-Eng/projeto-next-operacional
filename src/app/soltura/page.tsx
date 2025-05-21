0a504ac3def077d5b11fee676c95ed48164e7fae
Adriano-Reis-Eng <adrisilva.dev@gmail.com>
2025-05-21 13:18:33 -0300

    Correção DB

---
+++ src/app/soltura/page.tsx
@@ -0,0 +1,10 @@
+import AvisoForm from "../aviso/page";
+
+export default function Soltura(){
+    return (
+        <>
+            <AvisoForm/>
+        </>
+    )
+};
+    
--- src/app/uniformes/page.tsx
+++ src/app/uniformes/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { use, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Cookies from "js-cookie";
 import styles from "./uniforne.module.css";
 import InputBtn from "../components/inputBtn/InputBtn";
